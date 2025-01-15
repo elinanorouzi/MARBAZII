e2bb6ea3b510d7bca2933b63283b837bac522387
elinanorouzi <enoroozi75@gmail.com>
2025-01-15 19:34:20 +0330

    Update MARBAZI.py

--- MARBAZI.py
+++ MARBAZI.py
@@ -1,9 +1,8 @@
 #کتابخانه هاي مورد نياز
-#برای کنترل زماني و مقادير تصادفي براي غذا
 import turtle
 import time
 import random
-#سرعت بازي
+#کنترل سرعت بازي
 delay = 0.1
 score = 0
 high_score = 0
@@ -26,7 +25,6 @@ food.shape("circle")
 food.color("blue")
 food.penup()
 food.goto(0, 100)
-
 #نمايش امتياز
 pen = turtle.Turtle()
 pen.speed(0)
@@ -36,9 +34,10 @@ pen.hideturtle()
 pen.goto(0, 250)
 pen.write("Score : 0 High Score : 0", align="center", font=("candara", 24, "bold"))
 
-#ذخيره کردن بدن مار در يک ليست
+    #ليست ذخيره کردن بدن مار
 segments = []
-#توابع براي جهت مار
+
+#تابع براي جهت مار
 def go_up():
     if head.direction != "down":
         head.direction = "up"
@@ -80,7 +79,6 @@ wn.onkeypress(go_down, "s")
 wn.onkeypress(go_left, "a")
 wn.onkeypress(go_right, "d")
 
-#اين حلقه براي بروزرساني صفحه بازي به صورت مداوم
 while True:
     wn.update()
 
@@ -88,15 +86,15 @@ while True:
         time.sleep(1)
         head.goto(0, 0)
         head.direction = "stop"
-
-        #ريست شدن بازي
+        
         for segment in segments:
             segment.goto(1000, 1000)
         segments.clear()
+
+        
         score = 0
         delay = 0.1
 
-    #خوردن غذا و افزايش طول
     if head.distance(food) < 20:
         new_segment = turtle.Turtle()
         new_segment.speed(0)
@@ -105,37 +103,30 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
 
-        #جابجايي غذا
         x = random.randint(-270, 270)
         y = random.randint(-270, 270)
         food.goto(x, y)
 
-        #چاپ امتياز در بالاي صفحه
         score += 10
         if score > high_score:
             high_score = score
         delay -= 0.001
 
-    #حرکت بدن مار
     for index in range(len(segments) - 1, 0, -1):
-
-          
-        #بدست اوردن موقعيت بخش قبلي بدن مار
         x = segments[index - 1].xcor()
         y = segments[index - 1].ycor()
         segments[index].goto(x, y)
-    #منتقل کردن اولين بخش بدن مار به موقعيت سر مار
+
     if len(segments) > 0:
         x = head.xcor()
         y = head.ycor()
         segments[0].goto(x, y)
-    #بررسي کردن جهت فعلي مار
+
     move()
 
     pen.clear()
     pen.write("Score : {} High Score : {} ".format(score, high_score), align="center", font=("candara", 24, "bold"))
-    #مديريت تاخير
+
     time.sleep(delay)
 
-#جهت باز ماندن برنامه و پردازش ورودي ها و اجراي تغييرات
 wn.mainloop()
